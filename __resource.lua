f340c8421a85996d397e8727448bcf03c750cebe
xRadeCoding <49785124+xRadeCoding@users.noreply.github.com>
2022-09-09 19:47:08 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+
+description 'xRadeCoding-WeatherSync'
+
+author 'xRadeCoding'
+
+server_scripts {
+	"server/main.lua",
+}
+
+client_scripts {
+	"client/client.lua"
+}
+
